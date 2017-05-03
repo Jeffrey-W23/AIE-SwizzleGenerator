63f940c00c29343f20503c5e9ebdd93dbf5c9dfc
Thomas Wiltshire <thomas.wiltshire14@icloud.com>
2017-05-03 13:50:47 +1000

    First Commit.

---
+++ SwizzleGenerator/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	for (char x = 119; x < 123; x++)
+	{
+		for (char y = 119; y < 123; y++)
+		{
+			for (char z = 119; z < 123; z++)
+			{
+				for (char w = 119; w < 123; w++)
+				{
+					cout << "Vector4 Vector4::" << x << y << z << w << "()" << endl;
+					cout << "{" << endl;
+					cout << "	Vector4 result;" << endl;
+					cout << "	result.x = " << x << ";" << endl;
+					cout << "	result.y = " << y << ";" << endl;
+					cout << "	result.y = " << z << ";" << endl;
+					cout << "	result.y = " << w << ";" << endl;
+					cout << "	return result;" << endl;
+					cout << "}" << endl;
+				}
+			}
+		}
+	}
+}
